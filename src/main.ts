15d4a60e1d65d896f43124adba194a9fd1b857a1
Hugo Averty <hugoaverty@gmail.com>
2025-06-23 16:15:54 +0200

    Fixing

--- src/main.ts
+++ src/main.ts
@@ -4,20 +4,58 @@ import { bootstrapExtra } from "@workadventure/scripting-api-extra";
 
 console.log('Script started successfully');
 
-let currentPopup: any = undefined;
-
 // Waiting for the API to be ready
 WA.onInit().then(() => {
-    console.log('Scripting API ready');
-    console.log('Player tags: ',WA.player.tags)
+    WA.ui.actionBar.removeButton('roomListIcon');
+    WA.ui.actionBar.removeButton('invite-btn');
 
-    WA.room.area.onEnter('clock').subscribe(() => {
-        const today = new Date();
-        const time = today.getHours() + ":" + today.getMinutes();
-        currentPopup = WA.ui.openPopup("clockPopup", "It's " + time, []);
-    })
+    WA.player.setOutlineColor(158, 164, 171);
+    if (WA.player.tags.includes("admin")) {
+        WA.player.setOutlineColor(87, 137, 164);
+    }
+    if (WA.player.tags.includes("intervenant")) {
+        WA.player.setOutlineColor(255, 208, 0);
+    }
 
-    WA.room.area.onLeave('clock').subscribe(closePopup)
+    WA.ui.actionBar.addButton({
+        id: 'guide-btn',
+        // @ts-ignore
+        label: 'Guide d\'utilisation',
+        callback: () => {
+            WA.ui.modal.closeModal();
+                setTimeout(() => {
+                WA.ui.modal.closeModal();
+                WA.ui.modal.openModal({
+                    src: 'https://workadventu.re/wp-content/uploads/2025/06/Features-1.pdf',
+                    allow: 'fullscreen',
+                    title: 'Bienvenue',
+                    allowApi: true,
+                    position: 'center',
+                });
+            }, 200);
+            //WA.nav.openCoWebSite(WA.state.loadVariable('url_Bouton') as string);
+        }
+    });
+    WA.ui.actionBar.addButton({
+        id: 'plan-btn',
+        // @ts-ignore
+        label: 'Plan du monde',
+        callback: () => {
+            WA.ui.modal.closeModal();
+                setTimeout(() => {
+                WA.ui.modal.closeModal();
+                WA.ui.modal.openModal({
+                    src: 'https://workadventure.github.io/overview-liebherr/dist/',
+                    allow: 'fullscreen',
+                    title: 'Bienvenue',
+                    allowApi: true,
+                    position: 'center',
+                });
+            }, 200);
+            //WA.nav.openCoWebSite(WA.state.loadVariable('url_Bouton') as string);
+        }
+    });
+    
 
     // The line below bootstraps the Scripting API Extra library that adds a number of advanced properties/features to WorkAdventure
     bootstrapExtra().then(() => {
@@ -25,12 +63,5 @@ WA.onInit().then(() => {
     }).catch(e => console.error(e));
 
 }).catch(e => console.error(e));
-
-function closePopup(){
-    if (currentPopup !== undefined) {
-        currentPopup.close();
-        currentPopup = undefined;
-    }
-}
 
 export {};
