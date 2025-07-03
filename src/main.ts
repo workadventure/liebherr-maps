4f1d8c6f5591d1b655ebdd3a9a4a86c657ff596b
Hugo Averty <hugoaverty@gmail.com>
2025-07-03 15:42:54 +0200

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { bootstrapExtra } from "@workadventure/scripting-api-extra";
 console.log('Script started successfully');
 
 // Waiting for the API to be ready
-WA.onInit().then(() => { 
+WA.onInit().then(() => {
 
     WA.player.setOutlineColor(158, 164, 171);
     if (WA.player.tags.includes("admin")) {
