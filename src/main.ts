537ccd5e2b138e41efd620a8b568856457337787
Hugo Averty <hugoaverty@gmail.com>
2025-07-03 15:34:22 +0200

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
 /// <reference types="@workadventure/iframe-api-typings" />
 
 import { bootstrapExtra } from "@workadventure/scripting-api-extra";
-
+ 
 console.log('Script started successfully');
 
 // Waiting for the API to be ready
-WA.onInit().then(() => {
+WA.onInit().then(() => { 
 
     WA.player.setOutlineColor(158, 164, 171);
     if (WA.player.tags.includes("admin")) {
